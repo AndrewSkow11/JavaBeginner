97d47feea3ff956b684ebe32ecf6c0dd789ea8b5
Andrew Skow <skovorodnikov11@gmail.com>
2024-11-08 15:44:19 +0300

    page 22 of 193

---
+++ src/ch01_introduction/FirstVariable.java
@@ -0,0 +1,10 @@
+package ch01_introduction;
+
+public class FirstVariable {
+    public static void main(String[] args) {
+        String message = "Начальное значение";
+        System.out.println(message);
+        message = "Изменённое значение!";
+        System.out.println(message);
+    }
+}
---
+++ src/ch01_introduction/Hello.java
@@ -0,0 +1,13 @@
+package ch01_introduction;/*
+% java Hello.java
+Hello, World!
+
+javac Hello.java
+java Hello
+ */
+
+class Hello {
+    public static void main (String [] args){
+        System.out.println("Hello, World!");
+    }
+}
